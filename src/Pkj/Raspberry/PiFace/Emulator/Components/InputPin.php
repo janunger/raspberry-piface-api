529d9c0ca7844393d565cec0c435e8080d03605f
Jan Unger <ju@juit.de>
2015-04-25 21:48:44 +0200

    Feature: Can set the state of an emulator input pin

--- src/Pkj/Raspberry/PiFace/Emulator/Components/InputPin.php
+++ src/Pkj/Raspberry/PiFace/Emulator/Components/InputPin.php
@@ -11,4 +11,12 @@ class InputPin extends Pin implements \Pkj\Raspberry\PiFace\InputPin
     {
         return $this->stateProvider->readInput($this->pinIndex, $this->boardIndex) === 1;
     }
+
+    /**
+     * @param bool $flag
+     */
+    public function setIsOn($flag)
+    {
+        $this->stateProvider->writeInput($this->pinIndex, $this->boardIndex, $flag ? 1 : 0);
+    }
 }
